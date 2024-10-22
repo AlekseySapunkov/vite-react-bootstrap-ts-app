85a480dc44d4bf6768c4fdb6e270764e36b7cbd3
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-10-22 15:55:26 +0300

    delete footer column magazine

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -1,47 +1,39 @@
 import styles from "./Footer.module.scss"
-const Footer = (props:any) =>{
-  const modalHandler = ()=>{
+const Footer = (props: any) => {
+  const modalHandler = () => {
     props.setModalVisible();
   }
-    return  <footer className={styles.footer}>
+  return <footer className={styles.footer}>
     <div className={styles.container}>
-     <div className={styles.row}>
-       <div className={styles['footer-col']}>
-         <h4>Я и мои работы</h4>
-         <ul>
-           <li><a href="#">Обо мне</a></li>
-           <li><a href="#">Мои услуги</a></li>
-           <li><a href="#">Примеры работ</a></li>
-         </ul>
-       </div>
-       <div className={styles['footer-col']}>
-         <h4>Помощь</h4>
-         <ul>
-           <li><a href="#">FAQ</a></li>
-           <li><a href="#">Покупка</a></li>
-           <li><a href="#">Статус товара</a></li>
-           <li><a href="#">Варианты оплаты</a></li>
-           <li><a href="#">Скидки, промоакции</a></li>
-         </ul>
-       </div>
-       <div className={styles['footer-col']}>
-         <h4>Магазин</h4>
-         <ul>
-           <li><a href="#">Картины</a></li>
-           <li><a href="#">Мастерклассы</a></li>
-           <li><a href="#">CG</a></li>
-         </ul>
-       </div>
-       <div className={styles['footer-col']}>
-         <h4>Подписывайтесь и следите за новостями</h4>
-         <div className={styles['social-links']}>
-         <a href="https://t.me/your_name" target="_blank" title="Написать в Telegram" rel="noopener noreferrer" id={styles.telegram}></a>
-        <a href="#" target="_blank" title="Instagram" rel="noopener noreferrer" id={styles.instagram}></a>
-         </div>
-         <div title="Сделать заказ" className={styles['modal-button']} onClick={modalHandler}></div>
-       </div>
-     </div>
+      <div className={styles.row}>
+        <div className={styles['footer-col']}>
+          <h4>Я и мои работы</h4>
+          <ul>
+            <li><a href="#">Обо мне</a></li>
+            <li><a href="#">Мои услуги</a></li>
+            <li><a href="works">Примеры работ</a></li>
+          </ul>
+        </div>
+        <div className={styles['footer-col']}>
+          <h4>Помощь</h4>
+          <ul>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">Покупка</a></li>
+            <li><a href="#">Статус товара</a></li>
+            <li><a href="#">Варианты оплаты</a></li>
+            <li><a href="#">Скидки, промоакции</a></li>
+          </ul>
+        </div>
+        <div className={styles['footer-col']}>
+          <h4>Подписывайтесь и следите за новостями</h4>
+          <div className={styles['social-links']}>
+            <a href="https://t.me/your_name" target="_blank" title="Написать в Telegram" rel="noopener noreferrer" id={styles.telegram}></a>
+            <a href="#" target="_blank" title="Instagram" rel="noopener noreferrer" id={styles.instagram}></a>
+          </div>
+          <div title="Сделать заказ" className={styles['modal-button']} onClick={modalHandler}></div>
+        </div>
+      </div>
     </div>
- </footer>
+  </footer>
 }
 export default Footer
