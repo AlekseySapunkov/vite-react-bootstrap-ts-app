0c47f1deb4fe376fb189998812b835b61bb70a0d
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-10-22 13:52:25 +0300

    add Hook useNavigate

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,19 +1,17 @@
 import React from "react";
 import styles from "./Header.module.scss";
-import {  Nav } from "react-bootstrap";
-const Header = (props:any) => {
-
-  const setVisbilityContactsHandler = (event:any) =>{
-    event.preventDefault()
-    props.setContactsVisible()
+import { Nav } from "react-bootstrap";
+import { Outlet, useNavigate } from 'react-router-dom';
+const Header = (props: any) => {
+  const navigate = useNavigate();
+  const setVisbilityContactsHandler = (event: any) => {
+    navigate('/contacts', { replace: false })
   }
-  const setVisbilityMainContextHandler = (event:any) =>{
-    event.preventDefault()
-    props.setMainContextVisible()
+  const setVisbilityMainContextHandler = (event: any) => {
+    navigate('/', { replace: false })
   }
-  const setVisibilityMyWorksHandler = (event:any) =>{
-    event.preventDefault();
-    props.setMyWorksVisible()
+  const setVisibilityMyWorksHandler = (event: any) => {
+    navigate('/', { replace: false })
   }
   return (
     <React.Fragment>
@@ -23,13 +21,14 @@ const Header = (props:any) => {
           <h1>Художник Ксения</h1>
         </div>
         <Nav className="justify-content-center" variant="tabs" defaultActiveKey="#home">
-        <Nav.Item>
-            <Nav.Link href="#home" className={styles.navLink} onClick={setVisbilityMainContextHandler}>Главное меню</Nav.Link>
-        </Nav.Item>
-            <Nav.Link href="#works" className={styles.navLink} onClick={setVisibilityMyWorksHandler}>Мои работы</Nav.Link>
-            <Nav.Link href="#contacts" onClick={setVisbilityContactsHandler} className={styles.navLink}>Контакты для связи</Nav.Link>
-          </Nav>
+          <Nav.Item>
+            <Nav.Link href="/" className={styles.navLink} onClick={setVisbilityMainContextHandler}>Главное меню</Nav.Link>
+          </Nav.Item>
+          <Nav.Link href="works" className={styles.navLink} onClick={setVisibilityMyWorksHandler}>Мои работы</Nav.Link>
+          <Nav.Link href="contacts" onClick={setVisbilityContactsHandler} className={styles.navLink}>Контакты для связи</Nav.Link>
+        </Nav>
       </header>
+      <Outlet />
     </React.Fragment>
   );
 };
--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,7 @@ import ModalWindow from "../components/UI/ModalWindow";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [isContactsVisible, setContactsVisible] = useState(false);
-  const [isMainContextVisible, setMainContextVisible] = useState(true);
-  const [isMyWorksVisible, setMyWorksVisible] = useState(false);
   const [isModalVisible, setModalVisible] = useState(false);
-  const setVisibilityContactsHandler = () => {
-    setContactsVisible(true);
-    setMainContextVisible(false);
-    setMyWorksVisible(false);
-  };
-  const setVisibilityMainContextHandler = () => {
-    setContactsVisible(false);
-    setMainContextVisible(true);
-    setMyWorksVisible(false);
-  };
-  const setVisibilityMyWorksHandler = () => {
-    setContactsVisible(false);
-    setMainContextVisible(false);
-    setMyWorksVisible(true);
-  };
   const setModalVisibilityHandler = () => {
     setModalVisible(true);
     if (isModalVisible) {
@@ -40,15 +22,11 @@ function App() {
 
       <React.Fragment>
         {isModalVisible && <ModalWindow setModalVisible={setModalVisibilityHandler} />}
-        <Header
-          setContactsVisible={setVisibilityContactsHandler}
-          setMainContextVisible={setVisibilityMainContextHandler}
-          setMyWorksVisible={setVisibilityMyWorksHandler}
-        />
+        <Header />
         <Routes>
-          <Route path="/" element={isMainContextVisible && <MainContent />} />
-          <Route path="contacts" element={isContactsVisible && <Contacts setModalVisible={setModalVisibilityHandler} />} />
-          <Route path="works" element={isMyWorksVisible && <MyWorks />} />
+          <Route path="/" element={<MainContent />} />
+          <Route path="contacts" element={<Contacts setModalVisible={setModalVisibilityHandler} />} />
+          <Route path="works" element={<MyWorks />} />
         </Routes>
         <Footer setModalVisible={setModalVisibilityHandler} />
       </React.Fragment>
