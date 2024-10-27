600e53705eca76466f8b16e42da241322da61c48
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-10-27 15:48:31 +0300

    add style navbar to Header

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./Header.module.scss";
 import { Nav } from "react-bootstrap";
 import { Outlet, useNavigate } from 'react-router-dom';
-import { Container, Navbar } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
 const Header = (props: any) => {
   const navigate = useNavigate();
   const setVisbilityContactsHandler = (event: any) => {
@@ -24,25 +24,23 @@ const Header = (props: any) => {
           <div className={styles.logo}></div>
           <h1>Художник Ксения</h1>
         </div>
-        <Navbar expand="lg" >
-          <Container>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="justify-content-center" variant="tabs">
-                <Nav.Item>
-                  <Nav.Link href="/" className={styles.navLink} onClick={setVisbilityMainContextHandler}>Главное меню</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link href="works" className={styles.navLink} onClick={setVisibilityMyWorksHandler}>Мои работы</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link href="contacts" onClick={setVisbilityContactsHandler} className={styles.navLink}>Контакты для связи</Nav.Link>
-                </Nav.Item>
-                <Nav.Item><Nav.Link href="faq" onClick={setVisibilityFAQ} className={styles.navLink}>FAQ</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
+        <Navbar expand="lg" className={styles.navbar}>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="justify-content-center" variant="tabs">
+              <Nav.Item>
+                <Nav.Link href="/" className={styles.navLink} onClick={setVisbilityMainContextHandler}>Главное меню</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="works" className={styles.navLink} onClick={setVisibilityMyWorksHandler}>Мои работы</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="contacts" onClick={setVisbilityContactsHandler} className={styles.navLink}>Контакты для связи</Nav.Link>
+              </Nav.Item>
+              <Nav.Item><Nav.Link href="faq" onClick={setVisibilityFAQ} className={styles.navLink}>FAQ</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
       </header>
       <Outlet />
