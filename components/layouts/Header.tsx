e455af03df13b3ae4279dac3208a415da9ecc579
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-10-27 15:05:31 +0300

    add NavBAR

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import styles from "./Header.module.scss";
 import { Nav } from "react-bootstrap";
 import { Outlet, useNavigate } from 'react-router-dom';
-import HeaderButton from "../UI/HeaderButton";
+import { Container, Navbar, NavDropdown } from "react-bootstrap";
 const Header = (props: any) => {
   const [isMenuVisible, setMenuVisible] = useState(false);
   const navigate = useNavigate();
@@ -25,8 +25,30 @@ const Header = (props: any) => {
           <div className={styles.logo}></div>
           <h1>Художник Ксения</h1>
         </div>
-        {isMenuVisible && <HeaderButton />}
-        <Nav className="justify-content-center" variant="tabs" >
+        <Navbar expand="lg" className="bg-body-tertiary">
+          <Container>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        {/* <Nav className="justify-content-center" variant="tabs" >
           <Nav.Item>
             <Nav.Link href="/" className={styles.navLink} onClick={setVisbilityMainContextHandler}>Главное меню</Nav.Link>
           </Nav.Item>
@@ -39,7 +61,7 @@ const Header = (props: any) => {
           <Nav.Item>
             <Nav.Link href="faq" onClick={setVisibilityFAQ} className={styles.navLink}>FAQ</Nav.Link>
           </Nav.Item>
-        </Nav>
+        </Nav> */}
       </header>
       <Outlet />
     </React.Fragment>
