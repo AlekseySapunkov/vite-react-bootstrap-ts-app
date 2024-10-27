d2186fec8550551cffc32214d153726c29ed58b3
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-10-27 14:29:02 +0300

    MainContent fix grid

--- components/layouts/MainContent.tsx
+++ components/layouts/MainContent.tsx
@@ -1,29 +1,32 @@
 import styles from "./MainContent.module.scss";
+import { Container, Row, Col } from "react-bootstrap";
 const MainContent = (props: any) => {
   return (
     <main className={styles.main}>
-      <div className={styles.introduction}>
-        <div className={styles.textAboutMe}>
-          <h1>Коротко обо мне</h1>
-          <p>
-            Привет! Меня зовут Ксения Сидорова.
-            <br /> Я - художница и муза, а также путешественница, арт-персона.{" "}
-            <br />
-            Создаю произведения искусства, вдохновляю людей, двигаюсь в потоке
-            жизни.
-            <br /> Пою, танцую, играю, рисую.
-          </p>
-          <p>Я провожу мастер класы, рисую картины маслом <br />
-            А еще я могу делать афигенные 2d и 3d изображения.
-          </p>
-        </div>
-        <div className={styles.img}>
-          <img
-            src="me.jpg"
-            alt="imageFinger"
-          />
-        </div>
-      </div>
+      <Container className={styles.introduction}>
+        <Row>
+          <Col className={styles.textAboutMe} xs={12} md={8}>
+            <h1>Коротко обо мне</h1>
+            <p>
+              Привет! Меня зовут Ксения Сидорова.
+              <br /> Я - художница и муза, а также путешественница, арт-персона.{" "}
+              <br />
+              Создаю произведения искусства, вдохновляю людей, двигаюсь в потоке
+              жизни.
+              <br /> Пою, танцую, играю, рисую.
+            </p>
+            <p>Я провожу мастер класы, рисую картины маслом <br />
+              А еще я могу делать афигенные 2d и 3d изображения.
+            </p>
+          </Col>
+          <Col className={styles.img} xs={6} md={4}>
+            <img
+              src="me.jpg"
+              alt="Babka"
+            />
+          </Col>
+        </Row>
+      </Container>
     </main>
   );
 };
